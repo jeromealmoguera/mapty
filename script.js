92e509d4f11a0e3c5015430493369aafc3124eec
Jerome Almoguera <jeromealmoguera@gmail.com>
2025-03-11 15:53:13 +0800

    add navigator.geolocation api

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(
+        `https://www.google.com/maps/place/Jollibee+Tanay+Highway/@{latitude},{longitude}/data=!4m6!3m5!1s0x3397ebf7ea82a5d9:0xd7f19b614a82f970!8m2!3d14.500689!4d121.295322!16s%2Fg%2F12qg73zd0?entry=ttu&g_ep=EgoyMDI1MDMwNC4wIKXMDSoASAFQAw%3D%3D`
+      );
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
